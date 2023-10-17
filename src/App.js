315af4eeb96821afb5717429b9bc11bd4d63f60a
Penko Gadzhev <penko.gadzhev@digitall.com>
2023-10-17 12:23:59 +0300

    setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Home from './components/home';
+import Contact from './components/contact';
+import Navigation from './components/navigation';
+
+const App = () => {
+    return (
+        <div>
+            <h1>App</h1>
+            <hr />
+
+            <BrowserRouter>
+                <Navigation />
+                <Switch>
+                    <Route exact path='/' component={Home} />
+                    <Route path='/contact' component={Contact} />
+                </Switch>
+            </BrowserRouter>
+        </div>
+    );
+};
 
 export default App;
